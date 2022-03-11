f0105e6ecd2669b6ca8f1fa2f7edbc2d8b51fc27
Catherine Kawara <ndutakawara2@gmail.com>
2022-03-11 20:42:04 +0300

    ft:toggle feature - add toggle feature to services section

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(()=>{
+    $("#design").click(()=>{
+        $("#design .description").toggle();
+        $("#design img").toggle();
+    })
+    $("#development").click(()=>{
+        $("#development .description").toggle();
+        $("#development img").toggle();
+    })
+    $("#product").click(()=>{
+        $("#product .description").toggle();
+        $("#product img").toggle();
+    })
+})
