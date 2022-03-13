e5f7c409eb5c34d2667749aa41e508d88f3acd4a
Catherine Kawara <ndutakawara2@gmail.com>
2022-03-13 19:17:40 +0300

    fix:fix hover for all bug

--- js/index.js
+++ js/index.js
@@ -11,10 +11,52 @@ $(document).ready(()=>{
         $("#product .description").toggle();
         $("#product img").toggle();
     })
-    $(".port-item").hover(()=>{
-        $(".overlay").fadeIn();
+    $("#work4").hover(()=>{
+        $("#work4 .overlay").fadeIn();
     },()=>{
-        $(".overlay").fadeOut();
+        $("#work4 .overlay").fadeOut();
+
+    })
+    $("#work3").hover(()=>{
+        $("#work3 .overlay").fadeIn();
+    },()=>{
+        $("#work3 .overlay").fadeOut();
+
+    })
+    $("#work2").hover(()=>{
+        $("#work2 .overlay").fadeIn();
+    },()=>{
+        $("#work2 .overlay").fadeOut();
+
+    })
+    $("#work1").hover(()=>{
+        $("#work1 .overlay").fadeIn();
+    },()=>{
+        $("#work1 .overlay").fadeOut();
+
+    })
+    $("#work5").hover(()=>{
+        $("#work5 .overlay").fadeIn();
+    },()=>{
+        $("#work5 .overlay").fadeOut();
+
+    })
+    $("#work6").hover(()=>{
+        $("#work6 .overlay").fadeIn();
+    },()=>{
+        $("#work6 .overlay").fadeOut();
+
+    })
+    $("#work7").hover(()=>{
+        $("#work7 .overlay").fadeIn();
+    },()=>{
+        $("#work7 .overlay").fadeOut();
+
+    })
+    $("#work8").hover(()=>{
+        $("#work8 .overlay").fadeIn();
+    },()=>{
+        $("#work8 .overlay").fadeOut();
 
     })
 })
